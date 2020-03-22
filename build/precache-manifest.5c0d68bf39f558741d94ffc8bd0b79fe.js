cd04986b18f55d3a43657b0696d41bf769731035
Bastien P <bastien.pinard@ynov.com>
2020-03-22 19:07:48 +0100

    fix

---
+++ build/precache-manifest.5c0d68bf39f558741d94ffc8bd0b79fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a111c6b04802af5fb40f2482025c18ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1ce44406914198b28e2",
+    "url": "/static/css/main.36b3b3b2.chunk.css"
+  },
+  {
+    "revision": "1902d694f83f62d6cbb3",
+    "url": "/static/js/2.ddf4bc22.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ddf4bc22.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1ce44406914198b28e2",
+    "url": "/static/js/main.0d96ebfa.chunk.js"
+  },
+  {
+    "revision": "d0aa7463c3bb97daaf20",
+    "url": "/static/js/runtime-main.790eb67e.js"
+  }
+]);
